5047b3ac7c479778daef10d5afaa8fd59f625da1
Gaurav Gupta <gouravgupta840@gmail.com>
2019-04-18 11:19:03 +0530

    Routing Requests

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const http = require ('http');
 
 const server = http.createServer ((req, res) => {
-  //   console.log (req.url, req.method, req.headers);
-  //   process.exit ();
+  const url = req.url;
   res.setHeader ('Content-Type', 'text/html');
+  if (url === '/') {
+    res.write ('<html>');
+    res.write ('<head><title>Enter Message</title></head>');
+    res.write (
+      `<body>
+            <form action="/message" method="POST">
+                <input type="text" name="name">
+                <button type="submit">Send</button>
+            </form>
+        </body>`
+    );
+    res.write ('</html>');
+    return res.end ();
+  }
   res.write ('<html>');
-  res.write ('<head><title>Goonsroom</title></head>');
-  res.write ('<body><h1>Student Login</h1></body>');
+  res.write ('<head><title>Enter Message</title></head>');
+  res.write (`<body>Message</body>`);
   res.write ('</html>');
   res.end ();
 });
