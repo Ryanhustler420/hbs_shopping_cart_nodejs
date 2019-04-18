40f03ca69ed851def330020d8820d3aeb709ed9c
Gaurav Gupta <gouravgupta840@gmail.com>
2019-04-18 11:24:06 +0530

    Redirecting Requests

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const http = require ('http');
+const fs = require ('fs');
 
 const server = http.createServer ((req, res) => {
   const url = req.url;
+  const method = req.method;
   res.setHeader ('Content-Type', 'text/html');
   if (url === '/') {
     res.write ('<html>');
@@ -17,6 +19,13 @@ const server = http.createServer ((req, res) => {
     res.write ('</html>');
     return res.end ();
   }
+
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync ('message.txt', 'Dummy text');
+    res.writeHead (302, {Location: '/'});
+    return res.end ();
+  }
+
   res.write ('<html>');
   res.write ('<head><title>Enter Message</title></head>');
   res.write (`<body>Message</body>`);
