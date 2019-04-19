2661774ca8cd5ae81bc364a1f721c129f5b7a2fd
Gaurav Gupta <gouravgupta840@gmail.com>
2019-04-19 21:11:39 +0530

    Filtering Paths

--- routes/admin.js
+++ routes/admin.js
@@ -2,17 +2,18 @@ const express = require ('express');
 
 const router = express.Router ();
 
+// /admin/add-product => GET
 router.get ('/add-product', (req, res, next) => {
-  console.log ('In Product middleware');
   res.send (
-    `<form action="/product" method="POST"> 
-            <input type="text" name="title">
-            <button type="submit">Add Product</button>
-        </form>`
+    `<form action="/admin/add-product" method="POST"> 
+          <input type="text" name="title">
+          <button type="submit">Add Product</button>
+      </form>`
   );
 });
 
-router.post ('/product', (req, res, next) => {
+// /admin/add-product => POST
+router.post ('/add-product', (req, res, next) => {
   const {title} = req.body;
   console.log (title);
   res.redirect ('/');
