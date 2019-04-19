85895a871d6ba7c93d0e656b0eba34829460991f
Gaurav Gupta <gouravgupta840@gmail.com>
2019-04-19 21:29:58 +0530

    Returning a 404 Page

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require ('path');
+
 const express = require ('express');
 const bodyParser = require ('body-parser');
 
@@ -12,7 +14,9 @@ app.use ('/admin', adminRoutes);
 app.use (shopRoutes);
 
 app.use ((req, res, next) => {
-  res.status (404).send (`<h1>404</h1><h3>Dont Panic! Page not found!</h3>`);
+  res
+    .status (404)
+    .sendFile (path.join (__dirname, 'views', 'pageNotFound.html'));
 });
 
 app.listen (3000);
