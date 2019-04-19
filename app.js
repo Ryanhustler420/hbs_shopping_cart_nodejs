61cebd4d45cd22a0746c8313c7c9670350750355
Gaurav Gupta <gouravgupta840@gmail.com>
2019-04-19 21:04:17 +0530

    Using Express Router

--- app.js
+++ app.js
@@ -3,26 +3,12 @@ const bodyParser = require ('body-parser');
 
 const app = express ();
 
+const adminRoutes = require ('./routes/admin');
+const shopRoutes = require ('./routes/shop');
+
 app.use (bodyParser.urlencoded ({extended: false}));
 
-app.get ('/add-product', (req, res, next) => {
-  console.log ('In Product middleware');
-  res.send (
-    `<form action="/product" method="POST"> 
-        <input type="text" name="title">
-        <button type="submit">Add Product</button>
-    </form>`
-  );
-});
-
-app.post ('/product', (req, res, next) => {
-  const {title} = req.body;
-  console.log (title);
-  res.redirect ('/');
-});
-
-app.get ('/', (req, res, next) => {
-  res.send ('<h1>Hello From Express!</h1>');
-});
+app.use (adminRoutes);
+app.use (shopRoutes);
 
 app.listen (3000);
---
+++ routes/admin.js
@@ -0,0 +1,21 @@
+const express = require ('express');
+
+const router = express.Router ();
+
+router.get ('/add-product', (req, res, next) => {
+  console.log ('In Product middleware');
+  res.send (
+    `<form action="/product" method="POST"> 
+            <input type="text" name="title">
+            <button type="submit">Add Product</button>
+        </form>`
+  );
+});
+
+router.post ('/product', (req, res, next) => {
+  const {title} = req.body;
+  console.log (title);
+  res.redirect ('/');
+});
+
+module.exports = router;
