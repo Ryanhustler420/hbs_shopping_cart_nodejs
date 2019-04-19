e62be468006bf810b2ea3ae0fab273811b872617
Gaurav Gupta <gouravgupta840@gmail.com>
2019-04-19 15:26:19 +0530

    Using the Node Modules System

--- app.js
+++ app.js
@@ -1,47 +1,8 @@
 const http = require ('http');
-const fs = require ('fs');
+const {handler, someText} = require ('./routes');
 
-const server = http.createServer ((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  res.setHeader ('Content-Type', 'text/html');
-  if (url === '/') {
-    res.write ('<html>');
-    res.write ('<head><title>Enter Message</title></head>');
-    res.write (
-      `<body>
-            <form action="/message" method="POST">
-                <input type="text" name="message">
-                <button type="submit">Send</button>
-            </form>
-        </body>`
-    );
-    res.write ('</html>');
-    return res.end ();
-  }
+console.log (someText);
 
-  if (url === '/message' && method === 'POST') {
-    const body = [];
-
-    req.on ('data', chunk => {
-      body.push (chunk);
-    });
-
-    return req.on ('end', () => {
-      const parsedBody = Buffer.concat (body).toString ();
-      const message = parsedBody.split ('=')[1].split ('+').join (' ');
-      fs.writeFile ('message.txt', message, () => {
-        res.writeHead (302, {Location: '/'});
-        return res.end ();
-      });
-    });
-  }
-
-  res.write ('<html>');
-  res.write ('<head><title>Enter Message</title></head>');
-  res.write (`<body>Message</body>`);
-  res.write ('</html>');
-  res.end ();
-});
+const server = http.createServer (handler);
 
 server.listen (3000);
