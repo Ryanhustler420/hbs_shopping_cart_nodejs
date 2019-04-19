8ed233f32bdaff99702364d2115d833307a09205
Gaurav Gupta <gouravgupta840@gmail.com>
2019-04-19 12:24:22 +0530

    Blocking and Non-Blocking Code

--- app.js
+++ app.js
@@ -30,9 +30,10 @@ const server = http.createServer ((req, res) => {
     return req.on ('end', () => {
       const parsedBody = Buffer.concat (body).toString ();
       const message = parsedBody.split ('=')[1].split ('+').join (' ');
-      fs.writeFileSync ('message.txt', message);
-      res.writeHead (302, {Location: '/'});
-      return res.end ();
+      fs.writeFile ('message.txt', message, () => {
+        res.writeHead (302, {Location: '/'});
+        return res.end ();
+      });
     });
   }
 
