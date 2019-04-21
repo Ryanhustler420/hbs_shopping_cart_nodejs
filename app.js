35fd3e8eddc4a6d86deaea291363bd3a74a0311a
Gaurav Gupta <gouravgupta840@gmail.com>
2019-04-21 11:57:10 +0530

    Working with EJS

--- app.js
+++ app.js
@@ -2,19 +2,10 @@ const path = require ('path');
 
 const express = require ('express');
 const bodyParser = require ('body-parser');
-const Hbs = require ('express-handlebars');
 
 const app = express ();
 
-app.engine (
-  'handlebars',
-  Hbs ({
-    layoutsDir: 'views/layouts',
-    defaultLayout: 'main-layout',
-    extname: 'handlebars',
-  })
-);
-app.set ('view engine', 'handlebars');
+app.set ('view engine', 'ejs');
 app.set ('views', 'views'); //where to find the templates
 
 const adminData = require ('./routes/admin');
