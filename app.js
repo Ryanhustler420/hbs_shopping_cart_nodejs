70358a313c182dcb2dc9b138ee355d32801627e5
Gaurav Gupta <gouravgupta840@gmail.com>
2019-04-21 09:02:45 +0530

    Finishing the Pug Template

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use ('/admin', adminData.routes);
 app.use (shopRoutes);
 
 app.use ((req, res, next) => {
-  res.status (404).render ('pageNotFound', {pageDoc: 'Page not found'});
+  res.status (404).render ('pageNotFound', {pageTitle: 'Page not found'});
 });
 
 app.listen (3000);
--- routes/shop.js
+++ routes/shop.js
@@ -9,7 +9,7 @@ const router = express.Router ();
 
 router.get ('/', (req, res, next) => {
   const products = adminData.products;
-  res.render ('shop', {prods: products, docTitle: 'shop'});
+  res.render ('shop', {prods: products, pageTitle: 'shop', path: '/'});
 });
 
 module.exports = router;
