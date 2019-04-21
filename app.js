93600f6ba102a70413062f09599dfaa32f3223a1
Gaurav Gupta <gouravgupta840@gmail.com>
2019-04-21 16:22:06 +0530

    Finishing the Controllers

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require ('body-parser');
 
 const app = express ();
 
+const {error404} = require ('./controllers/error');
+
 app.set ('view engine', 'ejs');
 app.set ('views', 'views'); //where to find the templates
 
@@ -17,8 +19,6 @@ app.use (express.static (path.join (__dirname, 'public')));
 app.use ('/admin', adminRoutes);
 app.use (shopRoutes);
 
-app.use ((req, res, next) => {
-  res.status (404).render ('pageNotFound', {pageTitle: 'Page not found'});
-});
+app.use (error404);
 
 app.listen (3000);
