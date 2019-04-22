0ccdba608b9096ec8d9d10136fa15b1fe56d4b53
Gaurav Gupta <gouravgupta840@gmail.com>
2019-04-22 13:33:57 +0530

    Rendering the Product Detail View

--- controllers/shop.js
+++ controllers/shop.js
@@ -12,8 +12,13 @@ exports.getProductsList = (req, res, next) => {
 
 exports.getProductDetail = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById (prodId, console.log);
-  res.redirect ('/');
+  Product.findById (prodId, product => {
+    res.render ('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products',
+    });
+  });
 };
 
 exports.getIndex = (req, res, next) => {
