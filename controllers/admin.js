1d010e68a1be212718465dec5369b7d954069b63
Gaurav Gupta <gouravgupta840@gmail.com>
2019-04-25 20:51:20 +0530

    Storing the User in our Database

--- controllers/admin.js
+++ controllers/admin.js
@@ -12,7 +12,14 @@ exports.getAddProduct = (req, res, next) => {
 exports.postAddProduct = (req, res, next) => {
   const {title, imageUrl, price, description} = req.body;
   // const product = new Product (title, imageUrl, description, price);
-  const product = new Product (title, price, description, imageUrl);
+  const product = new Product (
+    title,
+    price,
+    description,
+    imageUrl,
+    null,
+    req.user._id
+  );
   product
     .save ()
     .then (result => {
