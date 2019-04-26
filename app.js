13ef2f6be9caf40383ea55c5da8d542fa314b5bd
Gaurav Gupta <gouravgupta840@gmail.com>
2019-04-26 21:51:52 +0530

    Adding and Using a User Model

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoose = require ('mongoose');
 const app = express ();
 
 const {error404} = require ('./controllers/error');
-// const {User} = require ('./models/user');
+const User = require ('./models/user');
 
 app.set ('view engine', 'ejs');
 app.set ('views', 'views'); //where to find the templates
@@ -18,14 +18,14 @@ const shopRoutes = require ('./routes/shop');
 app.use (bodyParser.urlencoded ({extended: false}));
 app.use (express.static (path.join (__dirname, 'public')));
 
-// app.use ((req, res, next) => {
-//   User.findById ('5cc1be260ccf9b2064f43f24')
-//     .then (user => {
-//       req.user = new User (user.name, user.email, user.cart, user._id);
-//       next ();
-//     })
-//     .catch (err => console.log (err));
-// });
+app.use ((req, res, next) => {
+  User.findById ('5cc32f4f06e75d0538eda38d')
+    .then (user => {
+      req.user = user;
+      next ();
+    })
+    .catch (err => console.log (err));
+});
 
 app.use ('/admin', adminRoutes);
 app.use (shopRoutes);
@@ -38,6 +38,18 @@ mongoose
     {useNewUrlParser: true}
   )
   .then (result => {
+    User.findOne ().then (user => {
+      if (!user) {
+        const user = new User ({
+          name: 'GauravGupta',
+          email: 'gouravgupta840@gmail.com',
+          cart: {
+            items: [],
+          },
+        });
+        user.save ();
+      }
+    });
     app.listen (3000);
   })
   .catch (err => {
