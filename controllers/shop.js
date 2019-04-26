455f6f1b16137a6d7f419a6fae84de98dbf46269
Gaurav Gupta <gouravgupta840@gmail.com>
2019-04-27 01:02:51 +0530

    Clearing the Cart After Storing an Order

--- controllers/shop.js
+++ controllers/shop.js
@@ -81,6 +81,9 @@ exports.postOrder = (req, res, next) => {
       order.save ();
     })
     .then (result => {
+      return req.user.clearCart ();
+    })
+    .then (() => {
       res.redirect ('/orders');
     })
     .catch (err => console.log (err));
