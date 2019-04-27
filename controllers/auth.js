d11722f40515705982275b97d332f33f5addb7c2
Gaurav Gupta <gouravgupta840@gmail.com>
2019-04-27 22:24:32 +0530

    Deleting a Cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,4 @@
 exports.getLogin = (req, res, next) => {
-  // const isLoggedIn =
-  // req.get ('Cookie').split (';')[1].trim ().split ('=')[1] === 'true';
-  // user can manupulate cookies data so that we should not store sensitive data
-  // into clients browser
-
-  console.log (req.session.isLoggedIn);
   res.render ('auth/login', {
     path: '/login',
     pageTitle: 'Login',
@@ -13,8 +7,13 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  // this isLoggedIn will be lose after the call finishesh
-  // res.setHeader ('Set-Cookie', 'loggedIn=true; Max-Age=10; Secure; HttpOnly');
   req.session.isLoggedIn = true;
   res.redirect ('/');
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy (err => {
+    console.log (err);
+    res.redirect ('/');
+  });
 };
