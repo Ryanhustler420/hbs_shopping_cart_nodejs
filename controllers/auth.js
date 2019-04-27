afbe7d9fdbe06d1258875041134ce8ddb702d8b3
Gaurav Gupta <gouravgupta840@gmail.com>
2019-04-27 12:23:23 +0530

    Configuring Cookies

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,19 @@
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn =
-    req.get ('Cookie').split (';')[1].trim ().split ('=')[1] === 'true';
+  // const isLoggedIn =
+  // req.get ('Cookie').split (';')[1].trim ().split ('=')[1] === 'true';
   // user can manupulate cookies data so that we should not store sensitive data
   // into clients browser
 
   res.render ('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: isLoggedIn,
+    isAuthenticated: false,
   });
 };
 
 exports.postLogin = (req, res, next) => {
   // this isLoggedIn will be lose after the call finishesh
-  res.setHeader ('Set-Cookie', 'loggedIn=true');
+  // res.setHeader ('Set-Cookie', 'loggedIn=true; Max-Age=10; Secure; HttpOnly');
+  res.setHeader ('Set-Cookie', 'loggedIn=true; HttpOnly');
   res.redirect ('/');
 };
