52e451f46ce22cca79cb8b1fe99332ebda762678
Gaurav Gupta <gouravgupta840@gmail.com>
2019-04-27 12:16:30 +0530

    Manipulating Cookies

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,9 @@
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn = req.get ('Cookie').split (';')[1].trim ().split ('=')[1];
+  const isLoggedIn =
+    req.get ('Cookie').split (';')[1].trim ().split ('=')[1] === 'true';
+  // user can manupulate cookies data so that we should not store sensitive data
+  // into clients browser
+
   res.render ('auth/login', {
     path: '/login',
     pageTitle: 'Login',
