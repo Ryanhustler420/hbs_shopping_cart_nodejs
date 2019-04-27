91e86a91aaa9e777369ae29a3660001756369fe1
Gaurav Gupta <gouravgupta840@gmail.com>
2019-04-27 12:14:04 +0530

    Setting a Cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,14 @@
 exports.getLogin = (req, res, next) => {
+  const isLoggedIn = req.get ('Cookie').split (';')[1].trim ().split ('=')[1];
   res.render ('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: req.isLoggedIn,
+    isAuthenticated: isLoggedIn,
   });
 };
 
 exports.postLogin = (req, res, next) => {
   // this isLoggedIn will be lose after the call finishesh
-  req.isLoggedIn = true;
+  res.setHeader ('Set-Cookie', 'loggedIn=true');
   res.redirect ('/');
 };
