7bfe7600ff09aaaa538922c1b824efd15b424393
Gaurav Gupta <gouravgupta840@gmail.com>
2019-05-06 10:30:00 +0530

    Checking For Field Equality

--- routes/auth.js
+++ routes/auth.js
@@ -38,7 +38,13 @@ router.post (
       'Please enter a password with only numbers and text and at least 5 characters.'
     )
       .isLength({min: 5})
-      .isAlphanumeric()
+      .isAlphanumeric(),
+    body('confirmPassword').custom((value, {req}) => {
+      if(value !== req.body.password){
+        throw new Error('Password have to match!');
+      }
+      return true;
+    })
   ],
   postSignup
 );
